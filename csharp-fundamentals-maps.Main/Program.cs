5d577a313dc95439fc69850a6c1b2d1cfe14a1c8
Nikita-vB <n.vanberendonk@gmail.com>
2023-11-28 13:06:33 +0100

    Core + extension

--- csharp-fundamentals-maps.Main/Program.cs
+++ csharp-fundamentals-maps.Main/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Dictionary<string, string> map = new Dictionary<string, string>();
+
+map.Add("firstName", "Nigel");
+map.Add("lastName", "Sibbert");
+map.Add("occupation", "Software Developer");
+
+//return map;
+Console.WriteLine(map["firstName"]);
+
