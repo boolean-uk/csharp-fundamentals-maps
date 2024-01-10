f206acbeb2f0d49bea6457056642748fcba919af
Louis <louisshrestha0412@outlook.com>
2024-01-10 15:52:05 +0100

    all tasks are completed

--- csharp-fundamentals-maps.Main/Program.cs
+++ csharp-fundamentals-maps.Main/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_fundamentals_maps.Main;
+
 Console.WriteLine("Hello, World!");
+Extension s = new Extension();
+s.LettersInName();
