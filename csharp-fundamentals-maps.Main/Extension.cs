2f28b79bfbfcd7e8330d9463ce834124d1099f07
Santhia <santhia.shanmukarasan@gmail.com>
2024-01-11 12:17:04 +0100

    maps exercise, done

--- csharp-fundamentals-maps.Main/Extension.cs
+++ csharp-fundamentals-maps.Main/Extension.cs
@@ -16,7 +16,7 @@ namespace csharp_fundamentals_maps.Main
             _planets = new Dictionary<string, float>();
             _planets.Add("Jupiter", 5.2f);
             _planets.Add("Uranus", 19.2f);
-            _planets.Add("Pluto", 39f);
+            //_planets.Add("Pluto", 39f);
             _planets.Add("Mercury", 0.39f);
             _planets.Add("Saturn", 9.54f);
             _planets.Add("Earth", 1f);
@@ -36,14 +36,16 @@ namespace csharp_fundamentals_maps.Main
             //          the planet name and the number of letters in its name
             //          iterate the _planets using a foreach object to load the result dictionary.
 
-            
+            foreach (KeyValuePair<string, float> planet in _planets)
+            {
+                result.Add(planet.Key, planet.Key.Length);
+            }
+
 
             return result;
         }
 
-
-
-
+        
 
 
         public Dictionary<string,float> OrderedPlanets()
@@ -52,22 +54,26 @@ namespace csharp_fundamentals_maps.Main
         }
         public Dictionary<string, float> OrderedPlanetsByDescending()
         {            
-            return _planets.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            return _planets.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
         }
         //TODO:  modify the OrderedPlanetsByDescending so it is not dictionary is not doing an OrderBy but OrderByDescending
 
 
+        
 
         //      
         //TODO   using the OrderedPlanets method get the
         //       furthest from the sun.
         //       Update the method to return the correct KeyValuePair's Key (the string)!
         //       Use the ClosestToTheSun as a guide
-        
+
 
         public string FurthestFromTheSun()
+
         {
-            return string.Empty;      
+            KeyValuePair<string, float> result = OrderedPlanets().Last();
+
+            return result.Key;      
         }
         public string ClosestToTheSun()
         {
@@ -75,6 +81,8 @@ namespace csharp_fundamentals_maps.Main
 
             return result.Key;
         }
+
+
 
         public Dictionary<string,float> Planets { get { return _planets; } }
 
